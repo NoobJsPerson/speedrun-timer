cfb6c2102c8728654f069bd60a1e61d0df58cb0f
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-04-19 21:46:53 +0000

    update new_run.js

--- new_run.js
+++ new_run.js
@@ -1,11 +1,12 @@
 function integerify(number){
   if(number.toString().match(/[0-9]+\.[0-9]9[0-9]+/)){
     return Math.floor(number*10)/10+0.1;
-  } else if(number == 0.5833333333333333){
-    return 0.59;
-  } else if(number.toString().match(/[0-9]+\.[0-9][0-9]9[0-9]+/)) {
+  } else if(number.toString().match(/[0-9]+\.[0-9][0-9]9[0-9]+/)){
     return Math.floor(number*100)/100+0.01;
-  } else return number;
+    }
+   else if(number ==0.5833333333333333) return 0.59;
+   else return number;
+   
 }
 // Load URL Params
 function getParameterByName(name, url = window.location.href) {
