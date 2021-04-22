36e6c4774333874e111e67f83b108a5723123c71
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-04-22 17:47:28 +0000

    update new_run.js

--- new_run.js
+++ new_run.js
@@ -14,8 +14,8 @@ function format(duration){
 function integerify(number){
 	// to fix increasing current frame in 60, 30 and 24 fps
   if(number.toString().match(/[0-9]+\.[0-9]9[0-9]+/)) return Math.floor(number*10)/10+0.1;
-   else if(number == 0.5833333333333333) return 0.59;
    else if(number.toString().match(/[0-9]+\.[0-9][0-9]9[0-9]+/)) return Math.floor(number*100)/100+0.01;
+   else if(number == 0.5833333333333333) return 0.59;
    else return number;
 }
 // Load URL Params
