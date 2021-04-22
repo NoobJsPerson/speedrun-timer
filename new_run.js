fdac465054c61630d2c6da6e7f7a3acf684fe9c5
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-04-22 17:45:20 +0000

    update new_run.js

--- new_run.js
+++ new_run.js
@@ -12,8 +12,7 @@ function format(duration){
   
 };
 function integerify(number){
-  if(number.toString().match(/[0-9]+\.[0-9]9[0-9]+/))
-    return Math.floor(number*10)/10+0.1;
+  if(number.toString().match(/[0-9]+\.[0-9]9[0-9]+/)) return Math.floor(number*10)/10+0.1;
    else if(number == 0.5833333333333333) return 0.59;
    else if(number.toString().match(/[0-9]+\.[0-9][0-9]9[0-9]+/)) return Math.floor(number*100)/100+0.01;
    else return number;
