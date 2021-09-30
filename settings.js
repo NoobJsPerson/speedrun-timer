31ae4c115aa448749ab5b2191b366aa42ef8bf56
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-09-30 16:19:57 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,20 @@
 let modMessage = 'Mod Message: time starts at ${start} and ends at ${end} with a framerate of ${framerate} fps to get a final time of ${timeStr}, retimed using [Better SpeedrunTimer](https://speedrun-timer.itsmeme11.repl.co)';
 let currentModMessage = localStorage.getItem("cmm");
+document.getElementById("cstmmsg").value = currentModMessage;
 if(!currentModMessage){
   localStorage.setItem("cmm",modMessage);
   currentModMessage = modMessage;
 }
-document.getElementById("cstmmsg").value = currentModMessage
+document.documentElement.setAttribute("theme", localStorage.getItem('theme'));
 function smm(event){
   if (event && event.target.value.replace(/\s/g, "") && event.target.value != modMessage){
     currentModMessage = event.target.value;
 		localStorage.setItem("cmm", currentModMessage);
   }
 }
+function changeMode() {
+  let wanted = localStorage.getItem("theme") == "dark" ? "light" : "dark"
+  document.documentElement.setAttribute("theme", wanted)
+  localStorage.setItem("theme", wanted);
+}
+
