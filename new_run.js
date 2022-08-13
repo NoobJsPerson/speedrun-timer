75749f9e8ad3f2ebb847bd95bd4906b33636f991
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2022-08-13 13:29:38 +0100

    Merge branch 'main' of https://github.com/NoobJsPerson/speedrun-timer

--- new_run.js
+++ new_run.js
@@ -1,132 +1,124 @@
-function interpolate(template, variables){
-	return template.replace(/\${[^{]+}/g, (match) => {
-		const path = match.slice(2, -1).trim();
-		return variables[path];
-	});
+function interpolate(template, letiables) {
+    return template.replace(/\${[^{]+}/g, (match) => {
+        const path = match.slice(2, -1).trim();
+        return letiables[path];
+    });
 }
 
-function format(duration){
-    let hours = ~~(duration/3600000);
- 
-    let minutes = ~~((duration % 3600000)/60000);
-    let seconds = ((duration % 60000)/1000).toFixed(3);
-    
-    minutes = minutes < 10 ? "0"+minutes: minutes;
-    seconds = seconds < 10 ? "0"+seconds: seconds;
+function format(duration) {
+    let hours = ~~(duration / 3600000);
 
-  return hours+":"+minutes+":"+seconds;
-  
+    let minutes = ~~((duration % 3600000) / 60000);
+    let seconds = ((duration % 60000) / 1000).toFixed(3);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    return hours + ":" + minutes + ":" + seconds;
+
 };
-// Load URL Params
-function getParameterByName(name, url = window.location.href) {
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
+// Initialise URL Params
+const searchParams = new URLSearchParams(window.location.search);
+const videoId = searchParams.get('id');
+// Load the IFrame Player API code asynchronously.
+let tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-const videoId = getParameterByName('id');
-    // Load the IFrame Player API code asynchronously.
-    var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
- 
 // Load page elements
-const totalTimeSpan = document.getElementById('total-time');
-const siteVideoIdInput = document.getElementById('video-id');
-const startValueInput = document.getElementById('start-value');
-const endValueInput = document.getElementById('end-value');
-const startSpan = document.getElementById('start');
-const goToStartButton = document.getElementById('go-to-start');
-const endSpan = document.getElementById('end');
-const goToEndButton = document.getElementById('go-to-end');
-const currentTimeSpan = document.getElementById('current-time');
-const modMessageText = document.getElementById("modMessage");
-const modMessageButton = document.getElementById("modMessageButton");
-const currentFrameSpan = document.getElementById('current-frame');
-let videoDiv = document.getElementById('video-div');
-const framerateElement = document.getElementById("framerate");
-const type = getParameterByName("type");
-let cmm = localStorage.getItem("cmm") || "Mod Message: time starts at ${start} and ends at ${end} with a framerate of ${framerate} fps to get a final time of ${timeStr}, retimed using [Better SpeedrunTimer](https://noobjsperson.github.io/speedrun-timer)";
+const totalTimeSpan = document.getElementById('total-time'),
+    siteVideoIdInput = document.getElementById('video-id'),
+    startValueInput = document.getElementById('start-value'),
+    endValueInput = document.getElementById('end-value'),
+    startSpan = document.getElementById('start'),
+    goToStartButton = document.getElementById('go-to-start'),
+    endSpan = document.getElementById('end'),
+    goToEndButton = document.getElementById('go-to-end'),
+    currentTimeSpan = document.getElementById('current-time'),
+    modMessageText = document.getElementById("modMessage"),
+    modMessageButton = document.getElementById("modMessageButton"),
+    currentFrameSpan = document.getElementById('current-frame'),
+    framerateElement = document.getElementById("framerate"),
+    type = searchParams.get("type")[0];
+let videoDiv = document.getElementById('video-div'),
+    cmm = localStorage.getItem("cmm") || "Mod Message: time starts at ${start} and ends at ${end} with a framerate of ${framerate} fps to get a final time of ${timeStr}, retimed using [Better SpeedrunTimer](https://noobjsperson.github.io/speedrun-timer)";
 // Create page variables
-var start = null;
-var end = null;
-var currentMillis = 0;
-var currentFrame= 0;
-var framerate = 30;
-let isLoaded = false;
+let start = null,
+    end = null,
+    currentMillis = 0,
+    currentFrame = 0,
+    framerate = 30,
+    isLoaded = false;
 
 // Apply Appearance Mode
 document.documentElement.setAttribute("theme", localStorage.getItem('theme'));
- 
+
 // Fallback Player
-var player = {
-    seekTo: function() {
+let player = {
+    seekTo: function () {
         throw 'unimplemented';
     },
-    pauseVideo: function() {
+    pauseVideo: function () {
         throw 'unimplemented';
     },
-    getCurrentTime: function() {
+    getCurrentTime: function () {
         throw 'unimplemented';
     },
-    playVideo: function() {
+    playVideo: function () {
         throw 'unimplemented';
     }
 };
 
-function validateFramerate (){
-  framerate = parseInt(framerateElement.value||framerate);
-  framerateElement.value = framerate;
+function validateFramerate() {
+    framerate = parseInt(framerateElement.value || framerate);
+    framerateElement.value = framerate;
 }
 
 function updateCurrentTime() {
     currentMillis = Math.floor(player.getCurrentTime() * 1000);
     currentFrame = Math.floor(player.getCurrentTime() * framerate);
 }
- 
+
 function setTime(millis) {
     player.pauseVideo();
     player.seekTo(millis);
 }
- 
+
 function stepBy(amount) {
     player.pauseVideo();
     updateCurrentTime();
-    setTime(Math.ceil(((currentFrame + amount)/framerate)*1000)/1000);
+    setTime(Math.ceil(((currentFrame + amount) / framerate) * 1000) / 1000);
 }
- function copyModMessage() {
-	// Allow user to copy mod message to clipboard
-	
-	modMessageText.focus();
-	modMessageText.select();
-	document.exec('copy');
-	alert(`The mod message has been copied to clipboard! Please paste it into the comment of the run you are verifying.`);
+function copyModMessage() {
+    // Allow user to copy mod message to clipboard
+
+    modMessageText.focus();
+    modMessageText.select();
+    document.exec('copy');
+    alert(`The mod message has been copied to clipboard! Please paste it into the comment of the run you are verifying.`);
 }
 function updateTotalTime() {
-    
-        // handle negative time I guess
-      if (start !== null && end !== null && start <= end) {
-        
-        
+
+    // handle negative time I guess
+    if (start !== null && end !== null && start <= end) {
+
+
         const endFrame = Math.round(end / 1000 * framerate);
         const startFrame = Math.round(start / 1000 * framerate);
         const frames = endFrame - startFrame;
-        
+
         let ms = ~~(frames * 1000 / framerate);
         let timeStr = format(ms);
         const params = {
-          start: format(start),
-          end: format(end),
-          timeStr,
-          framerate
+            start: format(start),
+            end: format(end),
+            timeStr,
+            framerate
         };
-        
-        const modMessage = interpolate(cmm,params);
+
+        const modMessage = interpolate(cmm, params);
         totalTimeSpan.innerHTML = timeStr;
         modMessageText.value = modMessage
 
@@ -134,18 +126,18 @@ function updateTotalTime() {
         modMessageText.disabled = false;
     }
 }
- 
+
 function showStart() {
     if (start === null) {
         return;
     }
- 
+
     startSpan.innerHTML = start;
     goToStartButton.style.visibility = "visible";
- 
-    
+
+
 }
- 
+
 function setStart() {
     updateCurrentTime();
     start = currentMillis;
@@ -153,22 +145,22 @@ function setStart() {
     showStart();
     updateTotalTime();
 }
- 
+
 function goToStart() {
-    setTime(start/1000);
+    setTime(start / 1000);
 }
- 
+
 function showEnd() {
     if (end === null) {
         return;
     }
- 
+
     endSpan.innerHTML = end;
     goToEndButton.style.visibility = "visible";
- 
-    
+
+
 }
- 
+
 function setEnd() {
     updateCurrentTime();
     end = currentMillis;
@@ -176,34 +168,34 @@ function setEnd() {
     showEnd();
     updateTotalTime();
 }
- 
-function goToEnd() {
-    setTime(end/1000);
-}
- 
 
- 
+function goToEnd() {
+    setTime(end / 1000);
+}
+
+
+
 function updateCurrentTimeSpan() {
     updateCurrentTime();
     currentTimeSpan.innerHTML = currentMillis;
     currentFrameSpan.innerHTML = currentFrame;
 }
- 
+
 function onPlayerReady() {
     videoDiv = document.getElementById('video-div');
     player.playVideo();
-    if(type == "t") setTimeout(() => framerateElement.value = twitch.getPlaybackStats().fps,6000);
+    if (type == "t") setTimeout(() => framerateElement.value = twitch.getPlaybackStats().fps, 6000);
     setInterval(updateCurrentTimeSpan, 50);
 }
- 
-// Load the player.
-if(type == "y"){
 
-    var youtube;
+// Load the player.
+if (type == "y") {
+
+    let youtube;
     function onYouTubePlayerAPIReady() {
         youtube = new YT.Player("video-div", {
             videoId: videoId,
-            playerVars:{
+            playerlets: {
                 rel: 0,
             },
             events: {
@@ -212,77 +204,77 @@ if(type == "y"){
                 'onStateChange': onYoutubeChange
             }
         });
-      setTimeout(() => {
-        if(isLoaded != true) onYoutubeError();
-      },5000);
-        
+        setTimeout(() => {
+            if (isLoaded != true) onYoutubeError();
+        }, 5000);
+
     }
 
     function onYoutubeChange(event) {
-      if(event.data == -1) isLoaded = true;
+        if (event.data == -1) isLoaded = true;
     }
 
     function onYoutubeError(event) {
-      console.log(event)
-      document.querySelector(".for-debug").style.display = "initial";
-      document.querySelector(".for-player").style.display = "none";
-      
+        console.log(event)
+        document.querySelector(".for-debug").style.display = "initial";
+        document.querySelector(".for-player").style.display = "none";
+
     }
- 
+
     function onYoutubeReady() {
-      console.log("ready")
+        console.log("ready")
         player = {
-            seekTo: function(timestamp) {
+            seekTo: function (timestamp) {
                 youtube.seekTo(timestamp);
             },
-            pauseVideo: function() {
+            pauseVideo: function () {
                 youtube.pauseVideo();
             },
-            getCurrentTime: function() {
+            getCurrentTime: function () {
                 return youtube.getCurrentTime();
             },
-            playVideo: function() {
+            playVideo: function () {
                 youtube.playVideo();
             }
         };
         onPlayerReady();
     }
 } else {
-  var twitch = new Twitch.Player("video-div", {
+    let twitch = new Twitch.Player("video-div", {
         video: videoId
     });
 
     player = {
         seekTo: function (timestamp) {
             player.playVideo();
-            setTimeout(function() {
+            setTimeout(function () {
                 twitch.seek(timestamp);
-                setTimeout(function() {
+                setTimeout(function () {
                     twitch.seek(timestamp);
                     player.pauseVideo();
                 }, 50)
             }, 50);
         },
         pauseVideo: function () {
-          twitch.pause();
+            twitch.pause();
         },
         getCurrentTime: function () {
             return twitch.getCurrentTime();
-		},
+        },
         playVideo: function () {
-          twitch.play(); 
+            twitch.play();
         }
     };
-    twitch.addEventListener(Twitch.Player.READY,onPlayerReady);
+    twitch.addEventListener(Twitch.Player.READY, onPlayerReady);
 }
-function computeFromDebugValues(){
+function computeFromDebugValues() {
 
 }
 function parseForTime(event) {
-   framerate = parseInt(document.getElementById('framerateAlt').value || framerate);
+    framerate = parseInt(document.getElementById('framerateAlt').value || framerate);
     const lct = parseFloat((JSON.parse(event.target.value)).lct);
-    if(isNaN(lct)) return;
-    if(event.target.id == "startobj") start = lct
+    if (isNaN(lct)) return;
+    if (event.target.id == "startobj") start = lct
     else end = lct
     document.getElementById(event.target.id).value = `${Math.floor(lct * framerate) / framerate}`;
 }
